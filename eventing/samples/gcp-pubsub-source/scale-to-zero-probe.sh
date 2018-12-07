cfb32b2dd612657fe2d3cc2d27c861c44bd3d72d
Joseph Burnett <josephburnett79@gmail.com>
2018-12-07 23:40:51 +0000

    Capture STDERR logs.

--- eventing/samples/gcp-pubsub-source/scale-to-zero-probe.sh
+++ eventing/samples/gcp-pubsub-source/scale-to-zero-probe.sh
@@ -9,16 +9,16 @@ while :
 do
     echo "Waiting for Revision to scale to zero (6 min)" | when
     sleep 360
-    kubectl get deploy | grep 'message-dumper' | when
+    kubectl get deploy 2>&1 | when
 
     echo "Publishing a random message and waiting for scale from zero (30 sec)" | when
     WANT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-    gcloud pubsub topics publish testing --message="$WANT"
+    gcloud pubsub topics publish testing --message="$WANT" 2>&1
     sleep 30
-    kubectl get deploy | grep 'message-dumper' | when
+    kubectl get deploy 2>&1 | when
 
     echo "Looking for the random message" | when
-    GOT=$(kubectl logs -l serving.knative.dev/service=message-dumper -c user-container | grep '{' | jq1.6 -r '.Data | @base64d' | grep "$WANT")
+    GOT=$(kubectl logs -l serving.knative.dev/service=message-dumper -c user-container | grep '{' | jq1.6 -r '.Data | @base64d' | grep "$WANT" 2>&1)
 
     echo "Got $GOT. Want $WANT." | when
 
